c20de05546af520c52b44cc08ed8c9cef0465bd9
yoyozbi <yohan.zbinden@gmail.com>
2019-08-06 18:48:19 +0200

    begin the workout for GUI

--- script.py
+++ script.py
@@ -1,17 +1,34 @@
-import numpy
+
+
+import numpy as np
 import sys
+import os
+scriptpath = "../imageEditor/AllModules.py"
+
+# Add the directory containing your module to the Python path (wants absolute paths)
+sys.path.append(os.path.abspath(scriptpath))
 import time
-from PIL import *
-def open_image(path):
-    image =[]
-    try:
-        image=Image.open(path)
-    except Exception as e:
-         print(e)
-    return image
-debut = time.time()
-image = open_image("/media/yohan/NIKON D80/DCIM/100NCD80/DSC_5202.NEF")
-image.show()
-fin = time.time()
-moyenne = fin-debut
-print("{}s".format(moyenne))
+from AllModules.py import open_image
+from tkinter.messagebox import *
+
+import cv2
+from tkinter import *
+
+
+
+mainWindow = Tk()
+img = ImageTk.PhotoImage(Image.open("/media/yohan/NIKON D80/DCIM/100NCD80/DSC_5204.NEF"))
+panel = tk.Label(root, image = img)
+panel.pack(side = center, fill = "both", expand = "yes")
+next = Button(text=">")
+previous = Button(text="<")
+next.pack(side=RIGHT)
+def callback():
+    if askyesno('Titre 1', 'Are you sure you want to move this file to trash'):
+        showwarning('Titre 2', 'Tant pis...')
+    else:
+        showinfo('Titre 3', 'Vous avez peur!')
+        showerror("Titre 4", "Aha")
+
+
+mainWindow.mainloop()
